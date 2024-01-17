867c1bcfe4c79e265ad0b2279cc7a935fad21e36
Victor Chen <victor.chen@mtzendo.com>
2024-01-17 20:54:05 +1300

    add the stock selector project

---
+++ Selector/main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import pandas as pd
+import numpy as np
+from tradingview_ta import TA_Handler, Interval, Exchange
+import yFinance as yf
+import mplFiance as mpf
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
