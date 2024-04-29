46951e31e399f57ed41cf44f2eab0ca573381e0d
Victor Chen <victor.chen@mtzendo.com>
2024-04-29 21:00:46 +1200

    import data from tradingview ta

--- Selector/main.py
+++ Selector/main.py
@@ -5,20 +5,55 @@
 
 import pandas as pd
 import numpy as np
-from tradingview_ta import TA_Handler, Interval, Exchange
-import yFinance as yf
-import mplFiance as mpf
-from datetime import datetime, timedelta
+
+import yfinance as yf
+import mplfinance as mpf
 import matplotlib.pyplot as plt
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+from tradingview_ta import TA_Handler, Interval, Exchange
+from datetime import datetime, timedelta
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    tickers = ['YPF',
+               'BBAR',
+               'GGAL',
+               'MELI',
+               'SUPV',
+               'BMA',
+               'DESP',
+               'LOMA',
+               'PAM',
+               'CEPU',
+               'TGS',
+               'TEO',
+               'BIOX',
+               'CRESY',
+               'IRS',
+               'EDN']
+    tickers_data = []
+    # Iterate through each ticker
+    for ticker in tickers:
+        print(ticker)
+        try:
+            data = TA_Handler(
+                symbol=ticker,
+                screener='america',
+                exchange='NYSE',
+                interval='1d'
+            )
+            data = data.get_analysis().summary
+            tickers_data.append(data)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        except Exception as e:
+            # If no data is found for the ticker in NUYS, search in NASDAQ
+            print(f"no data found for ticker { ticker } in NYSE. Searching in NASDAQ...")
+            data = TA_Handler(
+                symbol=ticker,
+                screener='america',
+                exchange='NASDAQ',
+                interval='1d'
+            )
+            data = data.get_analysis().summary
+            tickers_data.append(data)
+
+    print('Data successfully imported')
